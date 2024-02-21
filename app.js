51fa6356bdce32d6ea79bb0d26c5721c988ab4c4
uwindu <widucck21328@gmail.com>
2024-02-21 23:32:49 +0530

    posting data to Mailchimp's server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request =require("request");
+const https=require("https");
+
 
 const app = express();
 
@@ -9,16 +11,45 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html");
-})
+});
 
 app.post("/",function(req,res){
 
-    var firstName=req.body.fname;
-    var lastName=req.body.lname;
-    var email=req.body.email;
+    const firstName=req.body.fname;
+    const lastName=req.body.lname;
+    const email=req.body.email;
 
-    console.log(firstName,lastName,email);
-})
+    const data ={
+        members: [
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url ="https://usX.api.mailchimp.com/3.0/lists/<list id>";
+
+    const options = {
+        method:"POST",
+        auth:"<any string>:<Api key>"
+    }
+
+    const request = https.request(url, options,function(response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+});
 
 
 
@@ -26,3 +57,4 @@ app.listen(3000,function(){
 
     console.log("server running on port 3000")
 });
+
