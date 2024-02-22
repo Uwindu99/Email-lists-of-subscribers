ba7eb6e1f08225396543dd7996ca4a784b47c753
uwindu <widucck21328@gmail.com>
2024-02-22 11:55:30 +0530

    Adding success and failure pages

--- app.js
+++ app.js
@@ -34,21 +34,32 @@ app.post("/",function(req,res){
 
     const jsonData = JSON.stringify(data);
 
-    const url ="https://usX.api.mailchimp.com/3.0/lists/<list id>";
+    const url ="https://us22.api.mailchimp.com/3.0/lists/<ListId>";
 
     const options = {
         method:"POST",
-        auth:"<any string>:<Api key>"
+        auth:"<Any String>:<ApiKey>"
     }
 
     const request = https.request(url, options,function(response){
+
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+        }else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+
         response.on("data",function(data){
             console.log(JSON.parse(data));
-        })
-    })
+        });
+    });
 
-    request.write(jsonData);
+    
     request.end();
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/")
 });
 
 
@@ -57,4 +68,10 @@ app.listen(3000,function(){
 
     console.log("server running on port 3000")
 });
+
+
+
+
+
+
 
